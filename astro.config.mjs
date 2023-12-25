3b585927f3c9e1b1b38b2e47d6e25c173d42d41d
Tống Đức Thành Nam <tongducthanhnam@gmail.com>
2023-12-25 11:00:44 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+// import image from "@astrojs/image";
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://my-site.dev/',
+    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: false,
+    // Generate sitemap (set to "false" to disable)
+    integrations: [sitemap(), mdx(), lit()], // Add renderers to the config
+    // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
+    },
+});
